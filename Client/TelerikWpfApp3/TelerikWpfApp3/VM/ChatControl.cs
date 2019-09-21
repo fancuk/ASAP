ed596c3b7021d6829b5ded1885220da9cefd6346
이정환 <46296754+winterlood@users.noreply.github.com>
2019-09-22 08:17:01 +0900

    Update ChatControl.cs

--- Client/TelerikWpfApp3/TelerikWpfApp3/VM/ChatControl.cs
+++ Client/TelerikWpfApp3/TelerikWpfApp3/VM/ChatControl.cs
@@ -23,9 +23,26 @@ namespace TelerikWpfApp3.VM
             this.Chatdict[target].Add(chatitem);
         }
 
+        public void addChat(string target)
+        {
+            if (!this.Chatdict.ContainsKey(target))
+            {
+                ObservableCollection<Chatitem> inputTmp = new ObservableCollection<Chatitem>();
+                this.Chatdict.Add(target, inputTmp);
+            }
+        }
+
         public ObservableCollection<Chatitem> loadChat(string target)
         {
-            return this.Chatdict[target];
+            if (Chatdict.ContainsKey(target))
+            {
+                return this.Chatdict[target];
+            }
+            else
+            {
+                addChat(target);
+                return this.Chatdict[target];
+            }
         }
 
         public  void setChat(string target , ObservableCollection<Chatitem> chat)
