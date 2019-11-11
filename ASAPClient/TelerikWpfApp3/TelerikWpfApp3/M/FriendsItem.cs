b2b612b6fa0365551cf0e40106ee0594fb944de7
Favian <damin8@naver.com>
2019-11-12 03:50:44 +0900

    Update FriendsItem.cs
    - _status
    
    친구 상태 확인을 위한 _status 추가

--- ASAPClient/TelerikWpfApp3/TelerikWpfApp3/M/FriendsItem.cs
+++ ASAPClient/TelerikWpfApp3/TelerikWpfApp3/M/FriendsItem.cs
@@ -9,15 +9,20 @@ namespace TelerikWpfApp3.M
     public  class FriendsItem
     {
         private string _user;
+        private string _status;
         private string _lastMesseage;
 
-        public FriendsItem(string user, string lastMesseage)
+
+        public FriendsItem(string user, string lastMesseage, string _status)
         {
             User = user;
             LastMesseage = lastMesseage;
+            this._status = _status;
         }
 
         public string User { get => _user; set => _user = value; }
         public string LastMesseage { get => _lastMesseage; set => _lastMesseage = value; }
+
+        public string Status { get => _status; set => _status = value; }
     }
 }
