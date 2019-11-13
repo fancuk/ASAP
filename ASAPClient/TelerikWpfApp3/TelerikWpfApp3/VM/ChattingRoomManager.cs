2e14eed5e591f9cef90f8508e95d27621a5e80d0
Favian <damin8@naver.com>
2019-11-14 02:19:28 +0900

    Create ChattingRoomManager.cs
    채팅방 관리하는 매니저
    
    이를 통해 show, hide 등등
    
    이름에 맞는 채팅방 열고 닫기 가능(1개씩만)

---
+++ ASAPClient/TelerikWpfApp3/TelerikWpfApp3/VM/ChattingRoomManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TelerikWpfApp3.View;
+
+namespace TelerikWpfApp3.VM
+{
+    class ChattingRoomManager
+    {
+        private static ChattingRoomManager instance = null;
+
+        public static ChattingRoomManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ChattingRoomManager();
+                }
+                return instance;
+            }
+        }
+        private IDictionary<string, Window> chatRoom;
+
+        public void makeChatRoom(string target)
+        {
+            Window cR = new ChatRoom(target);
+            chatRoom.Add(target,cR);
+        }
+
+        public bool findChatRoom(string target)
+        {
+            if (chatRoom.ContainsKey(target))
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public void showChatRoom(string target)
+        {
+            chatRoom[target].Show();
+        }
+        
+        public void closeChatRoom(string target)
+        {
+            chatRoom[target].Hide();
+        }
+        public ChattingRoomManager()
+        {
+            chatRoom = new Dictionary<string, Window>();
+        }
+
+
+    }
+}
