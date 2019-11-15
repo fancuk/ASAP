c1d0622f5d778a3cbd313f12bb25985050a6b1e1
sg05138 <52522662+sg05138@users.noreply.github.com>
2019-11-15 19:04:13 +0900

    Update StartWindowViewModel.cs

--- ASAPClient/TelerikWpfApp3/TelerikWpfApp3/VM/StartWindowViewModel.cs
+++ ASAPClient/TelerikWpfApp3/TelerikWpfApp3/VM/StartWindowViewModel.cs
@@ -17,6 +17,7 @@ namespace TelerikWpfApp3.VM
         private UserControlViewModel _viewModel1;
         private UserControlViewModel1 _viewModel2;
         private ChatUserControlViewModel _chatViewModel;
+        private ChatControl chatControl;
         private string _myId;
 
         public string myId
@@ -33,28 +34,31 @@ namespace TelerikWpfApp3.VM
         }
         public ICommand Page1 { get; set; }
         public ICommand Page2 { get; set; }
-        public ICommand ChatPageOn { get; set; }
+        public ICommand ChatListPageOn { get; set; }
         public ICommand CloseCommand { get; set; }
         public ICommand LogOut { get; set; }
         public ICommand TalkPageOn { get; set; }
         public ICommand FriendsPageOn { get; set; }
+
         public StartWindowViewModel()
         {
             this._viewModel1 = new UserControlViewModel();
             this._viewModel2 = new UserControlViewModel1();
             this._chatViewModel = new ChatUserControlViewModel();
             this.friendsUserControlViewModel = FriendsUserControlViewModel.Instance;
+            this.chatControl = new ChatControl();
 
             FriendsPageOn = new Command(LoadFriendsPage, CE);
 
             Page1 = new Command(Page1Load, CE);
             Page2 = new Command(Page2Load, CE);
-            ChatPageOn = new Command(loadChatPage, CE);
+            ChatListPageOn = new Command(loadChatPage, CE);
             ContentView = null;
             // CloseCommand = new Command(ExecuteClose, CE);
             myId = ((App)Application.Current).myID;
             LogOut = new Command(logout, CE);
         }
+
         public void logout(object obj)
         {
             if (MessageBox.Show("로그아웃 하시겠습니까?",
@@ -70,7 +74,7 @@ namespace TelerikWpfApp3.VM
                 sw.Hide();
             }
         }
-          public void OnWindowClosing(object sender, CancelEventArgs e)
+        public void OnWindowClosing(object sender, CancelEventArgs e)
         {
             // Handle closing logic, set e.Cancel as needed
             e.Cancel = true;
@@ -91,7 +95,7 @@ namespace TelerikWpfApp3.VM
 
         private void loadChatPage(object obj)
         {
-            this.ContentView = this.friendsUserControlViewModel;
+            this.ContentView = this._chatViewModel;
         }
 
         private void LoadFriendsPage(object obj)
@@ -99,16 +103,15 @@ namespace TelerikWpfApp3.VM
             this.ContentView = this.friendsUserControlViewModel;
         }
 
-
-
         private void Page1Load(object obj)
         {
-              this.ContentView = this._viewModel1;
+            this.ContentView = this._viewModel1;
         }
         private void Page2Load(object obj)
         {
             this.ContentView = this._viewModel2;
         }
+
         public object ContentView
         {
             get { return this._contentView; }
