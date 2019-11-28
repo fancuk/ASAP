b37acb6de0f5493b8bb7021381ea13a7797b6314
Favian <damin8@naver.com>
2019-11-28 23:52:53 +0900

    Update SocketReciver.cs
    DispatchService 추가

--- ASAPClient/TelerikWpfApp3/TelerikWpfApp3/Networking/SocketReciver.cs
+++ ASAPClient/TelerikWpfApp3/TelerikWpfApp3/Networking/SocketReciver.cs
@@ -219,15 +219,18 @@ namespace TelerikWpfApp3.Networking
                             groupMemberList.Add(nameSlice[i]);
                         }
                         groupMemberListManager.AddGroupMemberList(groupIdx, groupMemberList);
-                        GroupChattingRoomManager.Instance.makeChatRoom(groupIdx);
                         if (maker == networkManager.MyId) // 만든 사람이 나라면
                         {
                             string plain = maker + "님이 채팅방을 만드셨습니다.";
-                            networkManager.SendData
+                            DispatchService.Invoke(() =>
+                            {
+                                GroupChattingRoomManager.Instance.makeChatRoom(groupIdx);
+                                networkManager.SendData
                                 ("<GSG>", maker + "/" + groupIdx + "/" + plain + "/" + time);
-                            groupChatManager.addChattingList(maker, groupIdx, plain, time); // 자신은 gsg를  안받기 때문에 추가해주기
-                            groupChatManager.addChat(groupIdx, new GroupChatItem(plain, maker, time, true)); // check가 true면 내가 보낸건가?
-                            GroupChattingRoomManager.Instance.showChatRoom(groupIdx);
+                                groupChatManager.addChattingList(maker, groupIdx, plain, time); // 자신은 gsg를  안받기 때문에 추가해주기
+                                groupChatManager.addChat(groupIdx, new GroupChatItem(plain, maker, time, true)); // check가 true면 내가 보낸건가?
+                                GroupChattingRoomManager.Instance.showChatRoom(groupIdx);
+                            });
                         }
                         //localDAO.GroupInfoCreate(groupIdx, groupName, maker + "^" + tokens[5]); // DAO에 넣어주는거 추가했습니다 BY 정구
                         // 그룹 채팅방 만들어주고 거기다가 maker가 만들었습니다 라고 메시지로 띄우기 (maker가 보낸 것 처럼)
