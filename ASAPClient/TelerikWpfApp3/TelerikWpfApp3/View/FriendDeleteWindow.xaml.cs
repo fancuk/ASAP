9125fdda2b6cce3d7000f65e084214e00980ca05
hjg0629 <hjg0629@gmail.com>
2019-12-01 19:58:06 +0900

    FriendDelete UI 추가염
    Yes OR No로 구분되는 UI

---
+++ ASAPClient/TelerikWpfApp3/TelerikWpfApp3/View/FriendDeleteWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TelerikWpfApp3.View
+{
+    /// <summary>
+    /// FriendAddWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class FriendDeleteWindow : Window
+    {
+        public FriendDeleteWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
